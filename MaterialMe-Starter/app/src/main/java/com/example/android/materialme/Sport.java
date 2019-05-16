70ee3195e1fd43564faf2c50ac2b0a664b57c5a5
Rtihai <rtihai@gmail.com>
2019-05-15 20:26:51 -0700

    Lab 05.2

--- MaterialMe-Starter/app/src/main/java/com/example/android/materialme/Sport.java
+++ MaterialMe-Starter/app/src/main/java/com/example/android/materialme/Sport.java
@@ -17,36 +17,47 @@
 package com.example.android.materialme;
 
 /**
- * Data model for each row of the RecyclerView.
+ * Data model for each row of the RecyclerView
  */
 class Sport {
 
-    //Member variables representing the title and information about the sport
+    // Member variables representing the title and information about the sport.
     private String title;
     private String info;
+    private final int imageResource;
 
     /**
-     * Constructor for the Sport data model
+     * Constructor for the Sport data model.
+     *
      * @param title The name if the sport.
      * @param info Information about the sport.
      */
-    Sport(String title, String info) {
+    public Sport(String title, String info, int imageResource) {
         this.title = title;
         this.info = info;
+        this.imageResource = imageResource;
     }
 
     /**
-     * Gets the title of the sport
+     * Gets the title of the sport.
+     *
      * @return The title of the sport.
      */
     String getTitle() {
         return title;
     }
+
     /**
-     * Gets the info about the sport
+     * Gets the info about the sport.
+     *
      * @return The info about the sport.
      */
     String getInfo() {
         return info;
     }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+
 }
